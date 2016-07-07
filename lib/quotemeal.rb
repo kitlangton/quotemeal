4f0ed8695847ffe613be844345ffefa4e00d7da0
Kit Langton <kitlangton@gmail.com>
2016-07-07 11:41:46 -0400

    okay

--- lib/quotemeal.rb
+++ lib/quotemeal.rb
@@ -1,7 +1,7 @@
 require "quotemeal/version"
 
 module Quotemeal
-  def do_it
+  def self.do_it
     resp = `curl -X POST --include 'https://andruxnet-random-famous-quotes.p.mashape.com/?cat=movies' -H 'X-Mashape-Key: Kh4Y3SbIMKmshCcDTCL5D57PnKmgp1BSXoQjsn4AvQcUdU107H' -H 'Content-Type: application/x-www-form-urlencoded' -H 'Accept: application/json' -s`
     quote = resp.split('"')[3]
     movie = resp.split('"')[7]
